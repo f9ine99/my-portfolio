c93d2cbc5f32bf8c3ac5299e231c81a3c9b59815
Firaol <f9ine99@gmail.com>
2026-03-22 04:00:35 +0300

    style(hero): remove Instagram link from socials

--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -21,10 +21,6 @@
         <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932L18.901 1.153ZM17.61 20.644h2.039L6.486 3.24H4.298L17.61 20.644Z" />
       </svg>
     </a>
-    <span class="inline-divider">|</span>
-    <a href="https://instagram.com/f9ine99" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-      <Instagram size={18} />
-    </a>
   </div>
   <div class="divider">|</div>
   <a href="/about" class="more">
